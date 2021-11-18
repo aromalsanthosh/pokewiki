6054a7d11b1eab8fcb4a0b0542216980dc2c5147
Aromal S <aromalsanthosh@protonmail.com>
2021-11-18 19:51:33 +0530

    initialization of project

---
+++ src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css'
+class App extends React.Component{
+
+    render(){
+        return(
+        <div>
+            Pokedex
+        </div>
+        )
+    }
+    
+}
+
+
+export default App;
