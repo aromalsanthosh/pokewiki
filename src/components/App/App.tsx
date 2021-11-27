5c804c7f8642cf7625f50ce2315c6aee1648d69c
Aromal S <aromalsanthosh@protonmail.com>
2021-11-27 10:50:06 +0530

    Finalizing State Schema

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,25 @@ import React from "react";
 import Pokewiki from "../Pokewiki/Pokewiki";
 import { pokemonData } from "../../data/pokemonData";
 import './App.css'
-class App extends React.Component{
+
+//interface AppState (general convention name of component followed by state)
+
+
+interface AppState {
+    searchField: string;
+    allPokemon: any;
+    searchPokemons: any;
+    selectedPokemon: any;
+}
+                                 //prop,state type
+class App extends React.Component <any,AppState>{
+
+    state = {
+        searchField: "",
+        allPokemon: [],
+        searchPokemons: [],
+        selectedPokemon: undefined
+    }
     componentDidMount(){
         const pokeData = pokemonData; 
     }
