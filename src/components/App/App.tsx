0b2108f7b7e891a9a4c8e4848a17f69d667f3fce
Aromal S <aromalsanthosh@protonmail.com>
2021-11-27 11:43:11 +0530

    Integrating the Pokemon Types

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import Pokewiki from "../Pokewiki/Pokewiki";
 import { pokemonData } from "../../data/pokemonData";
 import './App.css'
+import { PokemonSchema, PokemonSpritesSchema, UnpatchedPokemonSchema } from "../../types/pokemonSchema";
 
 //interface AppState (general convention name of component followed by state)
-
 
 interface AppState {
     searchField: string;
@@ -21,10 +21,38 @@ class App extends React.Component <any,AppState>{
         searchPokemons: [],
         selectedPokemon: undefined
     }
+
+    patchPokemonData = (pokemons : UnpatchedPokemonSchema[]):Pok => {
+        const patchedPokemons = pokemons.map((pokemon) => {
+            let parsedSprites: PokemonSpritesSchema = {
+                normal: undefined,
+                animated: undefined
+            };
+
+            try{
+                parsedSprites = pokemon.sprites &&  JSON.parse(pokemon.sprites);
+            }
+            catch(e){
+                console.log("Exception while parsing the sprites : ", e);
+            }
+
+            const patchedPokemon: PokemonSchema = {
+                ...pokemon,
+                sprites: parsedSprites
+            };
+
+            return patchedPokemon;
+        });
+
+        return patchedPokemons;
+        
+    }
+
     componentDidMount(){
         const pokeData = pokemonData;
         console.log(pokeData);
     }
+
     render(){
         return(
         <div className="App">
